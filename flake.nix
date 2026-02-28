5ee8896f57bfe146943ff9e3e408b9f1bf63b555
Miguel Vicente <oakymacintosh@duck.com>
2026-02-28 13:59:26 +0000

    Add nix flake and devcontainer config

--- flake.nix
+++ flake.nix
@@ -1,15 +1,67 @@
 {
-  description = "A very basic flake";
-
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
+  outputs = { self, nixpkgs }:
+    let
+      # Define the system architecture
+      system = "x86_64-linux"; # Replace with your system if different
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        # List of packages to be available in the shell
+        services.udev.packages = [];
+        packages = with pkgs; [
+          # Example packages: git and htop
+          rustc
+          cargo
+          gcc
+          androidndkPkgs_25.sdk
+          adb
+          android-studio
+          clang
+          pkg-config
+          gnustep.base
+          gnustep.gui
+          clangd
+          if pkgs.stdenv.isLinux then [ libobjc2 ] else [ ]
+          if pkgs.stdenv.isDarwin then [ darwin.apple_sdk.frameworks.Foundation ] else [ ]
+        ]
+        # Optional: Add a shell hook
+        shellHook = ''
+          export OBJCFLAGS="-I${pkgs.libobjc2}/include"
+          export LDFLAGS="-L${pkgs.libobjc2}/lib -lobjc" 
 
-  outputs = { self, nixpkgs }: {
+          export PS1=""
+        ''
+      };
+    };
 
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
+    config.android_sdk.accept_license = true;
+  overlays = [
+    (self: super: {
+      androidPkgs_8_0 = super.androidenv.composeAndroidPackages {
+        platformVersions = [ "26" ];
+        abiVersions = [ "x86" "x86_64" "aarch64"];
+      };
+    })
+  ];
 
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
+  (pkgs.buildFHSUserEnv {
+  name = "android-sdk-env";
+  targetPkgs = pkgs: (with pkgs;
+    [
+      androidPkgs_8_0.androidsdk
+      glibc
+    ]);
+  runScript = "bash";
+}).env
 
+   nixConfig = {
+    bash-prompt = "[pocketdarwin] \u@\h:\w$ ";
+    # Optional prefix/suffix
+    bash-prompt-prefix = "❄️  ";
   };
 }
+
